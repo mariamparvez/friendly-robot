9df7de9212627e86f0702f0d6b6c021c730e655d
Mariam Parvez <mariamparvez.m@gmail.com>
2021-08-05 16:57:22 +0530

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -424,47 +424,7 @@ Version      : 1.3
 			}]
 		});
 	}
-	//Home pharmacy slider
-	if($('.pharmacy-home-slider .swiper-container').length > 0) {
-		 	var swiper = new Swiper('.pharmacy-home-slider .swiper-container', {
-		 	loop: true,
-		 	speed: 1000,
-	      	pagination: {
-	        	el: '.pharmacy-home-slider .swiper-pagination',
-	        	dynamicBullets: true,
-	        	clickable: true,
-	      	},
-	      	navigation: {
-        		nextEl: '.pharmacy-home-slider .swiper-button-next',
-        		prevEl: '.pharmacy-home-slider .swiper-button-prev',
-      		},
-	    });
-	}
 	
-	// Date Range Picker
-	if($('.bookingrange').length > 0) {
-		var start = moment().subtract(6, 'days');
-		var end = moment();
-
-		function booking_range(start, end) {
-			$('.bookingrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-		}
-
-		$('.bookingrange').daterangepicker({
-			startDate: start,
-			endDate: end,
-			ranges: {
-				'Today': [moment(), moment()],
-				'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-				'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-				'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-				'This Month': [moment().startOf('month'), moment().endOf('month')],
-				'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-			}
-		}, booking_range);
-
-		booking_range(start, end);
-	}
 	// Chat
 
 	var chatAppTarget = $('.chat-window');
