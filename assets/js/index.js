bfc9b1917fc0d7bb6b810a9f65352ea84991429c
Mariam Parvez <mariamparvez.m@gmail.com>
2021-08-07 14:51:29 +0530

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ let searchBtns2 = document.getElementById("search-btns2");
 let searchBtns3 = document.getElementById("search-btns3");
 
 searchBtns.addEventListener("click",  myFunction => {
-    searchBtns.style.backgroundColor = "rgba(229,245,138, 0.5)";
+    searchBtns.style.backgroundColor = "rgba(224, 138, 245, 0.5)";
 	searchBtns2.style.backgroundColor = "rgba(138,165,255, 0.5)";
     searchBtns3.style.backgroundColor = "rgba(138,165,255, 0.5)";
 })
